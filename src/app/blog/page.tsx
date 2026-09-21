b35fe00260d86518fc1ae3d25d2e67b088f62310
vdevs777 <joaovsousamnm@gmail.com>
2026-09-21 14:05:57 -0300

    feat: add metadata for Blog and update layout metadata for Site.Set

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,28 @@
+import type { Metadata } from "next";
 import { BlogList } from "@/templates/blog";
 import { allPosts } from "contentlayer/generated";
+
+export const metadata: Metadata = {
+  title: "Blog",
+  description: "Dicas e stratégias para impulsionar seu negócio",
+  robots: "index, follow",
+  openGraph: {
+    title: "Blog",
+    description: "Dicas e stratégias para impulsionar seu negócio",
+    url: "https://site-blog-gamma.vercel.app/og-image.jpg",
+    siteName: "Site.Set",
+    locale: "pt_BR",
+    type: "website",
+    images: [
+      {
+        url: "https://site-blog-gamma.vercel.app/og-image.jpg",
+        width: 800,
+        height: 600,
+        alt: "Site.Set",
+      },
+    ],
+  },
+};
 
 export default function BlogListPage() {
   const sortedPosts = allPosts.sort(
