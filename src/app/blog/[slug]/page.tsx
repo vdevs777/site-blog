0324a0eafd6b8039496170403ad3f04d34ef5d4a
vdevs777 <joaovsousamnm@gmail.com>
2026-09-21 14:12:08 -0300

    feat: implement generateMetadata function for dynamic blog post metadata

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import { Post } from "@/templates/blog";
 import { allPosts } from "contentlayer/generated";
 import { notFound } from "next/navigation";
@@ -6,6 +7,26 @@ interface BlogPostPageProps {
   params: Promise<{
     slug: string;
   }>;
+}
+
+export async function generateMetadata({
+  params,
+}: BlogPostPageProps): Promise<Metadata> {
+  const { slug } = await params;
+
+  const post = allPosts.find((post) => post.slug === slug);
+
+  if (!post) return {};
+
+  return {
+    title: post.title,
+    description: post.description,
+    authors: [{ name: post.author.name }],
+    robots: "index, follow",
+    openGraph: {
+      images: [post.image],
+    },
+  };
 }
 
 export const revalidate = 60;
